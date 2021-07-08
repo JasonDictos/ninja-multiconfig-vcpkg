41b838e675c8a1e58d680f53fcdc8fc2447e4b85
Jason Dictos <jason@triceimaging.com>
2021-07-07 20:09:52 -0700

    Working problem

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <iostream>
+#include <boost/random/random_device.hpp>
+
+inline auto& generator() noexcept {
+	static thread_local boost::random_device generator{};
+	return generator;
+}
 
 int main(int argc, char **argv) {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << generator()() << std::endl;
 }
